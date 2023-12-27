cb885e73077dc32c420673d758c8e469ef436fe7
edabarsteigaite <145291058+edabarsteigaite@users.noreply.github.com>
2023-12-27 18:31:19 +0200

    Update testavimas.cpp

--- testavimas.cpp
+++ testavimas.cpp
@@ -3,7 +3,6 @@
 #include <chrono>
 #include <numeric>
 #include "studentas.h"
-#include "studentas_list.h"
 #include <fstream>
 #include <list>
 
@@ -12,42 +11,41 @@ using namespace std;
 void testavimas_vector(const string& failoPav, int skaicius, const string& kietiakaiF, const string& vargsiukaiF) {
     vector<StudentasV> studentaiV;
     vector<StudentasV> vargsiukai;
-    vector<StudentasV> kietiakai;
     vector<double> skaitymoLaikai;
     vector<double> rusiavimoLaikai;
     vector<double> isvedimoVLaikai;
     vector<double> isvedimoKLaikai;
 
-    int kartai = 5;
+    int kartai = 3;
     for (int i = 0; i < kartai; i++) {
-        auto start_nuskaitymo_laikas = std::chrono::high_resolution_clock::now();
+        auto start_nuskaitymo_laikas = std::chrono::steady_clock::now();
         skaitymo_laikas_vector(studentaiV, skaicius, failoPav);
-        auto end_nuskaitymas = std::chrono::high_resolution_clock::now();
+        auto end_nuskaitymas = std::chrono::steady_clock::now();
         std::chrono::duration<double> duration_nuskaitymas = end_nuskaitymas - start_nuskaitymo_laikas;
         skaitymoLaikai.push_back(duration_nuskaitymas.count());
 
         rusiavimas_vector(studentaiV);
 
-        auto start_rusiavimo_laikas = std::chrono::high_resolution_clock::now();
-        rusiavimo_laikas_vector(studentaiV, skaicius, vargsiukai, kietiakai);
-        auto end_rusiavimas = std::chrono::high_resolution_clock::now();
+        auto start_rusiavimo_laikas = std::chrono::steady_clock::now();
+        rusiavimo_laikas_vector(studentaiV, skaicius, vargsiukai);
+        auto end_rusiavimas = std::chrono::steady_clock::now();
         std::chrono::duration<double> duration_rusiavimas = end_rusiavimas - start_rusiavimo_laikas;
         rusiavimoLaikai.push_back(duration_rusiavimas.count());
 
-        auto start_vargsiuku_laikas = std::chrono::high_resolution_clock::now();
+        auto start_vargsiuku_laikas = std::chrono::steady_clock::now();
         isvedimo_vargsiukai_laikas_vector(vargsiukaiF, skaicius, vargsiukai);
-        auto end_vargsiukai = std::chrono::high_resolution_clock::now();
+        auto end_vargsiukai = std::chrono::steady_clock::now();
         std::chrono::duration<double> duration_vargsiukai = end_vargsiukai - start_vargsiuku_laikas;
         isvedimoVLaikai.push_back(duration_vargsiukai.count());
 
-        auto start_kietiaku_laikas = std::chrono::high_resolution_clock::now();
-        isvedimo_kietiakai_laikas_vector(kietiakaiF, skaicius, kietiakai);
-        auto end_kietiakai = std::chrono::high_resolution_clock::now();
+        auto start_kietiaku_laikas = std::chrono::steady_clock::now();
+        isvedimo_kietiakai_laikas_vector(kietiakaiF, skaicius, studentaiV);
+        auto end_kietiakai = std::chrono::steady_clock::now();
         std::chrono::duration<double> duration_kietiakai = end_kietiakai - start_kietiaku_laikas;
         isvedimoKLaikai.push_back(duration_kietiakai.count());
         studentaiV.clear();
         vargsiukai.clear();
-        kietiakai.clear();
+
     }
 
     double vidutinis_nuskaitymo_laikas = std::accumulate(skaitymoLaikai.begin(), skaitymoLaikai.end(), 0.0) / skaitymoLaikai.size();
@@ -65,42 +63,41 @@ void testavimas_vector(const string& failoPav, int skaicius, const string& kieti
 void testavimas_list(const string& failoPav, int skaicius, const string& kietiakaiF, const string& vargsiukaiF) {
     list<StudentasL> studentaiL;
     list<StudentasL> vargsiukai;
-    list<StudentasL> kietiakai;
     list<double> skaitymoLaikai;
     list<double> rusiavimoLaikai;
     list<double> isvedimoVLaikai;
     list<double> isvedimoKLaikai;
 
-    int kartai = 5;
+    int kartai = 3;
     for (int i = 0; i < kartai; i++) {
-        auto start_nuskaitymo_laikas = std::chrono::high_resolution_clock::now();
+        auto start_nuskaitymo_laikas = std::chrono::steady_clock::now();
         skaitymo_laikas_list(studentaiL, skaicius, failoPav);
-        auto end_nuskaitymas = std::chrono::high_resolution_clock::now();
+        auto end_nuskaitymas = std::chrono::steady_clock::now();
         std::chrono::duration<double> duration_nuskaitymas = end_nuskaitymas - start_nuskaitymo_laikas;
         skaitymoLaikai.push_back(duration_nuskaitymas.count());
 
         rusiavimas_list(studentaiL);
 
-        auto start_rusiavimo_laikas = std::chrono::high_resolution_clock::now();
-        rusiavimo_laikas_list(studentaiL, skaicius, vargsiukai, kietiakai);
-        auto end_rusiavimas = std::chrono::high_resolution_clock::now();
+        auto start_rusiavimo_laikas = std::chrono::steady_clock::now();
+        rusiavimo_laikas_list(studentaiL, skaicius, vargsiukai);
+        auto end_rusiavimas = std::chrono::steady_clock::now();
         std::chrono::duration<double> duration_rusiavimas = end_rusiavimas - start_rusiavimo_laikas;
         rusiavimoLaikai.push_back(duration_rusiavimas.count());
 
-        auto start_vargsiuku_laikas = std::chrono::high_resolution_clock::now();
+        auto start_vargsiuku_laikas = std::chrono::steady_clock::now();
         isvedimo_vargsiukai_laikas_list(vargsiukaiF, skaicius, vargsiukai);
-        auto end_vargsiukai = std::chrono::high_resolution_clock::now();
+        auto end_vargsiukai = std::chrono::steady_clock::now();
         std::chrono::duration<double> duration_vargsiukai = end_vargsiukai - start_vargsiuku_laikas;
         isvedimoVLaikai.push_back(duration_vargsiukai.count());
 
-        auto start_kietiaku_laikas = std::chrono::high_resolution_clock::now();
-        isvedimo_kietiakai_laikas_list(kietiakaiF, skaicius, kietiakai);
-        auto end_kietiakai = std::chrono::high_resolution_clock::now();
+        auto start_kietiaku_laikas = std::chrono::steady_clock::now();
+        isvedimo_kietiakai_laikas_list(kietiakaiF, skaicius, studentaiL);
+        auto end_kietiakai = std::chrono::steady_clock::now();
         std::chrono::duration<double> duration_kietiakai = end_kietiakai - start_kietiaku_laikas;
         isvedimoKLaikai.push_back(duration_kietiakai.count());
         studentaiL.clear();
         vargsiukai.clear();
-        kietiakai.clear();
+
     }
 
     double vidutinis_nuskaitymo_laikas = std::accumulate(skaitymoLaikai.begin(), skaitymoLaikai.end(), 0.0) / skaitymoLaikai.size();
