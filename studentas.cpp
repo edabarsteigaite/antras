3a09ba18e562e20fd92db6d072384b7379223f74
edabarsteigaite <145291058+edabarsteigaite@users.noreply.github.com>
2023-12-29 18:20:03 +0200

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -4,17 +4,21 @@
 #include <iostream>
 #include <sstream>
 
+
+using namespace std;
+
 int generuotiAtsitiktiniBalai(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
-bool lyginimasPagalVardus(const Studentas& a, const Studentas& b) {
-    if (a.vardas == b.vardas) {
-        return a.pavarde < b.pavarde;
+
+bool lyginimasPagalVardus(const StudentasV& a, const StudentasV& b) {
+    if (a.getVardas() == b.getVardas()) {
+        return a.getPavarde() < b.getPavarde();
     }
-    return a.vardas < b.vardas;
+    return a.getVardas() < b.getVardas();
 }
 
-double skaiciuotiGalutiniVidurki(const std::vector<int>& namu_darbai, int egzamino_rezultatas) {
+double skaiciuotiGalutiniVidurki(const vector<int>& namu_darbai, int egzamino_rezultatas) {
     double namuDarbuVidurkis = 0.0;
     if (!namu_darbai.empty()) {
         for (int rezultatas : namu_darbai) {
@@ -25,9 +29,9 @@ double skaiciuotiGalutiniVidurki(const std::vector<int>& namu_darbai, int egzami
     return 0.4 * namuDarbuVidurkis + 0.6 * egzamino_rezultatas;
 }
 
-double skaiciuotiGalutiniMediana(const std::vector<int>& namu_darbai, int egzamino_rezultatas) {
-    std::vector<int> surikiuoti_namu_darbai = namu_darbai;
-    std::sort(surikiuoti_namu_darbai.begin(), surikiuoti_namu_darbai.end());
+double skaiciuotiGalutiniMediana(const vector<int>& namu_darbai, int egzamino_rezultatas) {
+    vector<int> surikiuoti_namu_darbai = namu_darbai;
+    sort(surikiuoti_namu_darbai.begin(), surikiuoti_namu_darbai.end());
 
     if (surikiuoti_namu_darbai.size() % 2 == 0) {
         int vidurinis1 = surikiuoti_namu_darbai[surikiuoti_namu_darbai.size() / 2 - 1];
@@ -41,33 +45,160 @@ double skaiciuotiGalutiniMediana(const std::vector<int>& namu_darbai, int egzami
     }
 }
 
-void skaitytiDuomenisIsFailo(const std::string& failoPavadinimas, std::vector<Studentas>& studentai) {
-    std::ifstream failas(failoPavadinimas);
+void skaitytiDuomenisIsFailo(const string& failoPavadinimas, vector<StudentasV>& studentaiV) {
+    ifstream failas(failoPavadinimas);
     if (!failas.is_open()) {
-        std::cerr << "Klaida: Failas " << failoPavadinimas << " neegzistuoja arba negalima atidaryti." << std::endl;
+        cerr << "Klaida: Failas " << failoPavadinimas << " neegzistuoja arba negalima atidaryti." << endl;
         return;
     }
-    std::string eilute;
+    string eilute;
     getline(failas, eilute);
 
     while (getline(failas, eilute)) {
-        std::istringstream iss(eilute);
-        Studentas naujasStudentas;
+        istringstream iss(eilute);
+        StudentasV naujasStudentas;
 
-        iss >> naujasStudentas.vardas >> naujasStudentas.pavarde;
+        string vardas, pavarde;
+        iss >> vardas >> pavarde;
+        naujasStudentas.setVardas(vardas);
+        naujasStudentas.setPavarde(pavarde);
+
         int balas;
-
         while (iss >> balas) {
-            naujasStudentas.namu_darbai.push_back(balas);
+            naujasStudentas.getNamuDarbai().push_back(balas);
         }
 
-        if (naujasStudentas.namu_darbai.size() > 0) {
-            naujasStudentas.egzamino_rezultatas = naujasStudentas.namu_darbai.back();
-            naujasStudentas.namu_darbai.pop_back();
+        if (naujasStudentas.getNamuDarbai().size() > 0) {
+            naujasStudentas.setEgzaminoRezultatas(naujasStudentas.getNamuDarbai().back());
+            naujasStudentas.getNamuDarbai().pop_back();
         }
 
-        studentai.push_back(naujasStudentas);
+        studentaiV.push_back(naujasStudentas);
     }
 
     failas.close();
+}
+
+
+void ivedimasRanka(vector<StudentasV>& studentaiV) {
+    char testi;
+    do {
+        StudentasV naujasStudentas;
+
+        cout << "Iveskite studento varda: ";
+        string vardas;
+        cin >> vardas;
+        naujasStudentas.setVardas(vardas);
+
+        cout << "Iveskite studento pavarde: ";
+        string pavarde;
+        cin >> pavarde;
+        naujasStudentas.setPavarde(pavarde);
+
+        cout << "Iveskite namu darbu rezultatus (iveskite -1, jei norite baigti): ";
+
+
+        int namuDarbuRezultatas;
+        do {
+            while (true) {
+                if (!(cin >> namuDarbuRezultatas)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "Klaida: Netinkama ivestis. Iveskite balus nuo 1 iki 10: ";
+                }
+                else if (namuDarbuRezultatas == -1) {
+                    break;
+                }
+                else if (namuDarbuRezultatas < 1 || namuDarbuRezultatas > 10) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "Klaida: Netinkamas balas. Iveskite balus nuo 1 iki 10: ";
+                }
+                else {
+                    naujasStudentas.getNamuDarbai().push_back(namuDarbuRezultatas);
+                    break;
+                }
+            }
+        } while (namuDarbuRezultatas != -1);
+
+        cout << "Iveskite egzamino rezultata: ";
+        int egzaminoRezultatas;
+        while (true) {
+            if (!(cin >> egzaminoRezultatas)) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Klaida: Netinkama ivestis." << endl;
+            }
+            else if (egzaminoRezultatas < 1 || egzaminoRezultatas > 10) {
+                cout << "Klaida: Netinkamas balas." << endl;
+            }
+            else {
+                naujasStudentas.setEgzaminoRezultatas(egzaminoRezultatas);
+                break;
+            }
+        }
+
+        studentaiV.push_back(naujasStudentas);
+
+        cout << "Ar norite ivesti dar viena studenta? (t/n): ";
+        cin >> testi;
+    } while (testi == 't' || testi == 'T');
+}
+
+
+void ivedimasAtsitiktinai(vector<StudentasV>& studentaiV) {
+    char testi;
+    do {
+        StudentasV naujasStudentas;
+
+        cout << "Iveskite studento varda: ";
+        string vardas;
+        cin >> vardas;
+        naujasStudentas.setVardas(vardas);
+
+        cout << "Iveskite studento pavarde: ";
+        string pavarde;
+        cin >> pavarde;
+        naujasStudentas.setPavarde(pavarde);
+
+        int namuDarbuSkaicius = generuotiAtsitiktiniBalai(1, 10);
+        cout << "Generuoti atsitiktiniai namu darbu rezultatai (" << namuDarbuSkaicius << "): ";
+        for (int i = 0; i < namuDarbuSkaicius; i++) {
+            int balas = generuotiAtsitiktiniBalai(1, 10);
+            naujasStudentas.getNamuDarbai().push_back(balas);
+            cout << balas << " ";
+        }
+        cout << endl;
+
+        naujasStudentas.setEgzaminoRezultatas(generuotiAtsitiktiniBalai(1, 10));
+        cout << "Generuotas atsitiktinis egzamino rezultatas: " << naujasStudentas.getEgzaminoRezultatas() << endl;
+
+        studentaiV.push_back(naujasStudentas);
+
+        cout << "Ar norite ivesti dar viena studenta? (t/n): ";
+        cin >> testi;
+    } while (testi == 't' || testi == 'T');
+}
+
+
+void ivedimasIsFailo(vector<StudentasV>& studentaiV) {
+    string failoPavadinimas;
+    cout << "Iveskite failo pavadinima: ";
+    cin >> failoPavadinimas;
+
+    skaitytiDuomenisIsFailo(failoPavadinimas, studentaiV);
+}
+
+void spausdintiGalutiniusRezultatus(const vector<StudentasV>& studentaiV) {
+    cout << "\nGalutiniai rezultatai:\n";
+    cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
+    cout << string(70, '-') << endl;
+
+    for (const StudentasV& studentas : studentaiV) {
+        double galutinisVidurkiu = skaiciuotiGalutiniVidurki(studentas.getNamuDarbai(), studentas.getEgzaminoRezultatas());
+        double galutinisMediana = skaiciuotiGalutiniMediana(studentas.getNamuDarbai(), studentas.getEgzaminoRezultatas());
+
+        cout << setw(15) << studentas.getVardas() << setw(15) << studentas.getPavarde() << fixed << setprecision(2)
+            << setw(20) << galutinisVidurkiu << setw(20) << galutinisMediana << endl;
+    }
 }
